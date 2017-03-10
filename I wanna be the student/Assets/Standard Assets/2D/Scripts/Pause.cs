e25f7f6f7c570ea61c31c967fa959331d9330b1b
Van'ka <bahbok777@gmail.com>
2017-03-10 21:45:57 +0200

    Added Pause
    Added Pause and PauseMenu

---
+++ I wanna be the student/Assets/Standard Assets/2D/Scripts/Pause.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+
+public class Pause : MonoBehaviour
+{
+
+    public GUISkin myGUI;
+    public GUIStyle style;
+
+    private Blur BF;
+    private int window = 20;
+    private int ScreenWidth;
+    private int ScreenHeight;
+    private bool menu = false;
+
+    void Start()
+    {
+        BF = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Blur>();
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!menu)
+            {
+
+                BF.enabled = true;
+                Time.timeScale = 0;
+                menu = true;
+                ScreenWidth = Screen.width;
+                ScreenHeight = Screen.height;
+                window = 0;
+            }
+            else
+            {
+                window = 20;
+                BF.enabled = false;
+                menu = false;
+                Time.timeScale = 1;
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.skin = myGUI;
+        if (menu)
+        {
+            if (window == 0)
+            {
+                if (GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2 - 120, 170, 30), "CONTINUE"))
+                {
+                    window = 20;
+                    menu = false;
+                    Time.timeScale = 1;
+                    BF.enabled = false;
+                }
+
+                if (GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2 - 80, 170, 30), "SCOREBOARD"))
+                {
+                   
+                }
+
+                if (GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2 - 40, 170, 30), "MANUAL"))
+                {
+
+                }
+
+                if (GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2 - 0, 170, 30), "EXIT"))
+                {
+                    Application.Quit();
+                }
+            }
+        }
+    }
+}
